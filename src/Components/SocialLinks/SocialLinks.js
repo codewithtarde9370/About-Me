f60d0b6f9684a4981c59865c0516a5ae547003a1
codewithtarde9370 <teja36491@gmail.com>
2024-09-16 20:26:53 +0530

    media queries added

--- src/Components/SocialLinks/SocialLinks.js
+++ src/Components/SocialLinks/SocialLinks.js
@@ -49,13 +49,23 @@ const links= [
 ]
 
   return (
-    <div className='flex flex-col top-[35%] left-0 fixed'>
+    <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
         <ul>
             {
-                links.map(({id,child,style,href})=>{
+                links.map(({id,child,style,href,download})=>{
                     return (
-                        <li key={id} className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500'>
-                <a href={href} className='w-full flex justify-between items-center text-white'>{child}</a>
+                        <li key={id} 
+                        className=
+                            "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500"
+                            >
+                <a 
+                href={href} 
+                className='w-full flex justify-between items-center text-white'
+                download={download}
+                target='_blank'
+                rel='noreferrer'>
+                    {child}
+                    </a>
                 </li>
                     )
                 })
