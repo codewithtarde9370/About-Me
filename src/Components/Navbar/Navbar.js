5e1d23e8881b14d0fe09e7b719ff3e9decde5652
codewithtarde9370 <teja36491@gmail.com>
2024-09-16 18:44:24 +0530

    removed link

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { FaBars, FaTimes } from "react-icons/fa";
 
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 function Navbar() {
     const [nav, setNav] = useState(false)
