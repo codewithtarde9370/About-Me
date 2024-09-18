92d0d050f6371ca9d03c8e17fd3f938892a3a0f1
codewithtarde9370 <teja36491@gmail.com>
2024-09-18 09:38:31 +0530

    responsiveness

--- src/Views/Portfolio/Portfolio.js
+++ src/Views/Portfolio/Portfolio.js
@@ -22,7 +22,10 @@ function Portfolio() {
   return (
     <div 
     name="portfolio"
-    className='text-white h-screen w-full bg-gradient-to-b from-black via-black to-gray-700 md:h-screen sm:min-h-full'>
+    className="text-white w-full bg-gradient-to-b from-black via-black to-gray-700 min-h-screen md:h-screen"
+  >
+    {/* text-white h-screen w-full bg-gradient-to-b from-black via-black to-gray-700 md:h-screen sm:min-h-full */}
+  
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
